b2cf88aa186cfac7ca2243d6464ae0b6b4e9d582
Dawno <utkarshsinghcs@gmail.com>
2018-07-23 23:21:14 +0530

    hackerearth challenge

---
+++ sumset.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int m,n;
+ cin>>n;
+ int nrr[n];
+ for(int i=0;i<n;i++){
+   cin>>nrr[i];
+ }
+ cin>>m;
+ int mrr[m];
+ for(int i=0;i<m;i++){
+   cin>>mrr[i];
+ }
+}

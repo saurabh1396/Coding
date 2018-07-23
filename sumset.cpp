fea93882476553be1b1afee5662c479b86ceed5c
Dawno <utkarshsinghcs@gmail.com>
2018-07-24 03:18:50 +0530

    completed

--- sumset.cpp
+++ sumset.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
   int m,n;
@@ -11,5 +12,12 @@ int main(){
  int mrr[m];
  for(int i=0;i<m;i++){
    cin>>mrr[i];
+ }
+ sort(nrr,nrr+n);
+ sort(mrr,mrr+m);
+ int s= mrr[0]-nrr[0];
+ int e= mrr[m-1]-nrr[n-1];
+ for(int i=s;i<=e;i++){
+   cout<<i<<" ";
  }
 }
